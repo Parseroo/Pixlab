7992cc0f83453add28b300ce664bb6f86f95edbc
Parseroo <kfussell21@dtechhs.org>
2019-02-06 10:02:06 -0800

    added in the package com.gradescope.pixlab line, but for some reason my changes aren't showing up on github.

--- classes/ColorChooser.java
+++ classes/ColorChooser.java
@@ -9,16 +9,14 @@ import java.awt.Color;
  * 
  * @author Barb Ericson ericson@cc.gatech.edu
  */
-public class ColorChooser 
-{
+public class ColorChooser {
   
   /**
    * Method to let the user pick a color and return
    * the color object. 
    * @return the picked color or red if no color was picked
    */
-  public static Color pickAColor()
-  {
+  public static Color pickAColor() {
     Color color = Color.white;
     
     // create a JFrame to be the parent of the color chooser open dialog
@@ -34,8 +32,7 @@ public class ColorChooser
   }
   
   /** Main method for testing the ColorChooser */
-  public static void main(String[] args)
-  {
+  public static void main(String[] args) {
     Color pickedColor = ColorChooser.pickAColor();
     System.out.println(pickedColor);
   }
